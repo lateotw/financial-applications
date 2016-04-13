8d8e2bdbd54b0d51f36443e47562c3c60f7f844a
lateotw <fay.zhao@gmail.com>
2016-04-13 09:26:49 -0400

    added call pricer

--- pricer.py
+++ pricer.py
@@ -1,1 +1,51 @@
-# place holder
+from math import log, sqrt, exp
+from scipy import stats
+
+class call_option(object):
+    ''' European call options, standard Black-Scholes
+
+    S0 : stock price
+    K : strike price
+    T : maturity in years
+    r : risk free rate
+    sigma : volatility
+
+    '''
+
+    def __init__(self, S0, K, T, r, sigma):
+        self.S0 = float(S0)
+        self.K = K
+        self.T = T
+        self.r = r
+        self.sigma = sigma
+
+    def value(self):
+
+        d1 = ((log(self.S0 / self.K)
+            + (self.r + 0.5 * self.sigma ** 2) * self.T)
+            / (self.sigma * sqrt(self.T)))
+        d2 = ((log(self.S0 / self.K)
+            + (self.r - 0.5 * self.sigma ** 2) * self.T)
+            / (self.sigma * sqrt(self.T)))
+        value = (self.S0 * stats.norm.cdf(d1, 0.0, 1.0)
+            - self.K * exp(-self.r * self.T) * stats.norm.cdf(d2, 0.0, 1.0))
+        return value
+
+    def vega(self):
+
+        d1 = ((log(self.S0 / self.K)
+            + (self.r + 0.5 * self.sigma ** 2) * self.T)
+            / (self.sigma * sqrt(self.T)))
+        vega = self.S0 * stats.norm.pdf(d1, 0.0, 1.0) * sqrt(self.T)
+        return vega
+
+    def imp_vol(self, C0, sigma_est=0.2, it=100):
+
+        option = call_option(self.S0, self.K, self.T, self.r, sigma_est)
+        for i in range(it):
+            option.sigma -= (option.value() - C0) / option.vega()
+        return option.sigma
+
+
+class put_option(object):
+    pass
